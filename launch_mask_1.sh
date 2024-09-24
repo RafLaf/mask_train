a0f2744d5f9b80212293e63d177808163fc74557
raflaf <ralafarg@gmail.com>
2024-09-24 17:29:18 +0930

    typo

--- launch_mask_1.sh
+++ launch_mask_1.sh
@@ -9,7 +9,7 @@ CONFIG_PATH="./configs/mask/mask_dtd.yaml"
 
 
 # Run the Python script for each configuration
-for i in {0..49} #loop trhough the tak number
+for i in {0..49} #loop trhough the task number
 do
     python main_one_task.py --cfg ${CONFIG_PATH} ${i}
 done
