cd30fefea8fdd84c96220bd8c7344510137c72e4
RafLaf <ralafarg@gmail.com>
2024-10-24 13:50:46 +0200

    vary shots

--- optuna_lora.py
+++ optuna_lora.py
@@ -2,6 +2,8 @@ import subprocess
 import yaml
 import optuna
 import argparse
+import logging
+import numpy as np
 
 def extract_metric(output):
     # Split the output into lines and search for the "Best accuracy" line
@@ -36,29 +38,79 @@ def objective(trial, config_file):
     
     # Extract the output from stdout and find the best accuracy
     output = process.stdout
-    print(output)
     result = extract_metric(output)
-    print(f'\n \n Result obtained {result} \n \n with \n lr_lora: {lr_lora} \n reg_lora: {reg_lora} \n epoch_lora: {epoch_lora}')
     return result
+
+def update_config(config_file, num_support):
+    # Load the existing config.yaml
+    with open(config_file, 'r') as f:
+        config = yaml.safe_load(f)
+    
+    # Update the number of support in the appropriate section
+    config['DATA']['TEST']['EPISODE_DESCR_CONFIG']['NUM_SUPPORT'] = num_support
+    
+    # Save the updated config.yaml
+    with open(config_file, 'w') as f:
+        yaml.safe_dump(config, f)
+
+
+def log_best_results(study, config_file, shots):
+    # Get the best trial
+    best_trial = study.best_trial
+
+    # Create a dictionary with the best trial's parameters and value
+    best_data = {
+        'num_shots': shots,
+        'best_accuracy': best_trial.value,
+        'lr_lora': best_trial.params['lr_lora'],
+        'reg_lora': best_trial.params['regularization_lora'],
+        'epoch_lora': best_trial.params['epoch_lora'],
+        'config_file': config_file
+    }
+
+    # Log the best trial at the end of optimization
+    logging.info(f'Best trial: {best_data}')
+
+    print(f"Best trial:\n {best_data}")
 
 def main():
     # Parse command-line arguments
     parser = argparse.ArgumentParser(description="Optuna hyperparameter optimization for LoRA model")
     parser.add_argument('config_file', type=str, help='Path to the config file')
     parser.add_argument('--n_trials', type=int, default=50, help='Number of Optuna trials')
-    
+    parser.add_argument('--vary_shots', action='store_true', help='Vary the number of shots from 5 to 65 in increments of 5')
+    parser.add_argument('--n_shots', type=int, default=50, help='Number of shots to use in testing')
+    parser.add_argument('--log_file', type=str, help='Path to the log file')
+
     args = parser.parse_args()
 
-    # Run the optimization using Optuna
-    study = optuna.create_study(direction="maximize")
-    study.optimize(lambda trial: objective(trial, args.config_file), n_trials=args.n_trials)
+    # Set up logging
+    logging.basicConfig(filename=args.log_file, level=logging.INFO, 
+                        format='%(asctime)s - %(levelname)s - %(message)s')
+    logging.info('Started Optuna optimization')
 
-    # Print the best trial and its hyperparameters
-    print("Best trial:")
-    trial = study.best_trial
-    print(f"  Value: {trial.value}")
-    print(f"  Params: {trial.params}")
-    print(f"  Config file: {args.config_file}")
+    if args.vary_shots:
+        #for s in range(5, 70, 5):
+        for s in range(1, 5):
+            update_config(args.config_file, s)
+            study = optuna.create_study(direction="maximize")
+            study.optimize(lambda trial: objective(trial, args.config_file), n_trials=args.n_trials)
+            
+            # Log best results
+            best_trial = study.best_trial
+            logging.info(f'Best trial for {s} shots: Value: {best_trial.value}, Params: {best_trial.params}')
+            print(f'Best trial for {s} shots:\n Value: {best_trial.value}\n Params: {best_trial.params}')
+            log_best_results(study, args.config_file, s)
+    else:
+        update_config(args.config_file, args.n_shots)
+        study = optuna.create_study(direction="maximize")
+        study.optimize(lambda trial: objective(trial, args.config_file), n_trials=args.n_trials)
+        
+        # Log best results
+        best_trial = study.best_trial
+        logging.info(f'Best trial: Value: {best_trial.value}, Params: {best_trial.params}')
+        print(f'Best trial:\n Value: {best_trial.value}\n Params: {best_trial.params}')
+        log_best_results(study, args.config_file, args.n_shots)
 
 if __name__ == "__main__":
     main()
--- optuna_mask.py
+++ optuna_mask.py
@@ -2,6 +2,9 @@ import subprocess
 import yaml
 import optuna
 import argparse
+import numpy as np
+import logging
+
 
 def extract_metric(output):
     # Split the output into lines and search for the "Best accuracy" line
@@ -40,24 +43,73 @@ def objective(trial, config_file):
     print(f'\n \n Result obtained {result} \n \n with \n lr_mask: {lr_mask} \n reg_mask: {reg_mask} \n epoch_mask: {epoch_mask}')
     return result
 
-def main():
-    # Parse command-line arguments
-    parser = argparse.ArgumentParser(description="Optuna hyperparameter optimization for MASK model")
-    parser.add_argument('config_file', type=str, help='Path to the config file')
-    parser.add_argument('--n_trials', type=int, default=50, help='Number of Optuna trials')
-
-    args = parser.parse_args()
-
-    # Run the optimization using Optuna
+# Run the optimization using Optuna
+def do_study(config_file, n_trials):
     study = optuna.create_study(direction="maximize")
-    study.optimize(lambda trial: objective(trial, args.config_file), n_trials=args.n_trials)
+    study.optimize(lambda trial: objective(trial, config_file), n_trials=n_trials)
 
     # Print the best trial and its hyperparameters
     print("Best trial:")
     trial = study.best_trial
     print(f"  Value: {trial.value}")
     print(f"  Params: {trial.params}")
-    print(f"  Config file: {args.config_file}")
+    print(f"  Config file: {config_file}")
+    return study
+
+
+def log_best_results(study, config_file, shots):
+    # Get the best trial
+    best_trial = study.best_trial
+
+    # Create a dictionary with the best trial's parameters and value
+    best_data = {
+        'num_shots': shots,
+        'best_accuracy': best_trial.value,
+        'lr_mask': best_trial.params['lr_mask'],
+        'reg_mask': best_trial.params['reg_mask'],
+        'epoch_mask': best_trial.params['epoch_mask'],
+        'config_file': config_file
+    }
+
+    # Log the best trial at the end of optimization
+    logging.info(f'Best trial: {best_data}')
+
+    print(f"Best trial:\n {best_data}")
+
+
+def main():
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser(description="Optuna hyperparameter optimization for MASK model")
+    parser.add_argument('config_file', type=str, help='Path to the config file')
+    parser.add_argument('--n_trials', type=int, default=50, help='Number of Optuna trials')
+    parser.add_argument('--vary_shots', action='store_true', help='loop though the nu;ber of shots')  # <-- New boolean flag
+    parser.add_argument('--n_shots', type=int, default=50, help='number of shots')
+    parser.add_argument('--log_file', type=str, help='Path to the config file')
+
+    args = parser.parse_args()
+
+    logging.basicConfig(filename=args.log_file, level=logging.INFO, 
+                        format='%(asctime)s - %(levelname)s - %(message)s')
+    logging.info('Started Optuna optimization')
+
+    def update_config(config_file, num_support):
+        with open(config_file, 'r') as f:
+            config = yaml.safe_load(f)
+        print(config['DATA']['TEST']['EPISODE_DESCR_CONFIG']['NUM_SUPPORT'])
+        config['DATA']['TEST']['EPISODE_DESCR_CONFIG']['NUM_SUPPORT'] = num_support
+        with open(config_file, 'w') as f:
+            yaml.safe_dump(config, f)
+
+    if args.vary_shots:
+        #for s in range(5, 70, 5):
+        for s in range(1, 5):
+            update_config(args.config_file, s)
+            study = do_study(args.config_file, args.n_trials)
+            log_best_results(study, args.config_file, s)
+    else:
+        update_config(args.config_file, args.n_shots)
+        study = do_study(args.config_file, args.n_trials)
+        log_best_results(study, args.config_file , args.n_shots)
 
 if __name__ == "__main__":
     main()
